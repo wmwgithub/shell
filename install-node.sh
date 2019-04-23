61de59665b5526af2c0e80b380e0b1e8068b8cb3
王旻伟 <3209304627@qq.com>
2019-04-23 20:00:58 +0800

    fix:自动添加环境变量

--- install-node.sh
+++ install-node.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #设定要安装的node版本
-version='v10.15.1'
+version='v10.15.3'
 #拉取node安装包
 sudo wget https://nodejs.org/dist/${version}/node-${version}-linux-x64.tar.xz
 sudo tar -xvf node-${version}-linux-x64.tar.xz
+rm -rf node-${version}-linux-x64.tar.xz
 #重新命名文件夹
 sudo mv node-${version}-linux-x64 nodejs
 #拿到当前目录
 LocalPath=$(pwd)
-#创建软链接
-sudo ln -s ${LocalPath}/nodejs/bin/node /usr/local/bin/node
-sudo ln -s ${LocalPath}/nodejs/bin/npm /usr/local/bin/npm
+#配置环境变量
+sudo echo export PATH=$PATH:${LocalPath}/nodejs/bin>>/etc/profile
+source /etc/profile
 
