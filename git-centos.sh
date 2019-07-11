d67e6c8928d201453aa4ffb384d94acd4710a4c8
王旻伟 <3209304627@qq.com>
2019-07-11 23:05:16 +0800

    feat:自动安装新版本git

---
+++ git-centos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#指定git版本
+LocalPath=$(pwd)
+GIT_VERSION='2.22.0'
+#安装git 依赖库
+yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel
+yum install -y gcc perl-ExtUtils-MakeMaker
+yum install -y autoconf automake libtool
+下载
+wget https://github.com/git/git/archive/v${GIT_VERSION}.tar.gz
+# 解压
+tar -zvxf v${GIT_VERSION}.tar.gz
+# 安装 
+cd git-$GIT_VERSION
+make configure
+./configure --prefix=/opt/git
+make
+make install
+echo "export PATH=$PATH:/opt/git/bin" >> /etc/profile
+# 回到运行此代码的目录 删除下载的安装包
+cd $LocalPath
+rm -rf git-${GIT_VERSION} *.tar.gz*
+#移除本身或者安装带来的低版本git 
+sudo yum remove -y git
+source /etc/profile
+git --version
